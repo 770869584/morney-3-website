7a798f657dbe2f52bd80f40c849d6c99744baebb
oudaxiong <770869584@qq.com>
2022-04-05 23:45:02 +0800

    deploy

---
+++ precache-manifest.b624d8fe5987c1a834b47641d33b6a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f23c1c6af42c07ca84",
+    "url": "/morney-3-website/css/app.7dec4c66.css"
+  },
+  {
+    "revision": "1ea105d48630acf2e9f7a98ca1df13e6",
+    "url": "/morney-3-website/index.html"
+  },
+  {
+    "revision": "60f23c1c6af42c07ca84",
+    "url": "/morney-3-website/js/app.7a21e3bf.js"
+  },
+  {
+    "revision": "c7f240da47eef4b1946d",
+    "url": "/morney-3-website/js/chunk-vendors.b226d691.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/morney-3-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-3-website/robots.txt"
+  }
+]);
